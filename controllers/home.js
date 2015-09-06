0f9bb2015e20d9d56d9999e5448160c3dbe05032
Christopher Walker <cnwalker@uchicago.edu>
2015-09-06 07:27:36 -0400

    Added state and federal reps

--- controllers/home.js
+++ controllers/home.js
@@ -8,52 +8,53 @@
 
 exports.index = function(req, res) {
   if (req.user) {
+    var state_rep = null;
+    var congressman = null;
     var user = req.user;
     if(user.profile.latitude && user.profile.longitude) {
       console.log('http://congress.api.sunlightfoundation.com/legislators/locate?' + 'latitude=' + user.profile.latitude +
       '&' + 'longitude=' + user.profile.longitude + '&' + 'apikey=' + Sunlight_API_KEY);
       urllib.request('http://congress.api.sunlightfoundation.com/legislators/locate?' + 'latitude=' + user.profile.latitude +
-      '&' + 'longitude=' + user.profile.longitude + '&' + 'apikey=' + Sunlight_API_KEY, function(err, data, res) {
+      '&' + 'longitude=' + user.profile.longitude + '&' + 'apikey=' + Sunlight_API_KEY, function(err, data, resp) {
         if (err) {
           console.log("error! ", err);
         }
        var results = JSON.parse(data.toString()).results;
-       var senator_one = null;
-       var senator_two = null;
-       var congressman = null;
 
        for (var i = 0; i < results.length; i++) {
-         if (results[i].chamber === 'senate') {
-           if (senator_one) {
-             senator_two = results[i];
-           } else {
-             senator_one = results[i];
-           }
-         }
-
          if (results[i].chamber === 'house') {
            congressman = results[i];
          }
        }
 
-       console.log('senator_one:', senator_one, 'senator_two:', senator_two,
-       'congressman', congressman);
-
        if(!err) {
-         res.render('adopt/neighborhood', {
-           title: 'Your Neighborhood',
-           senator_one: senator_one,
-           senator_two: senator_two,
-           congressman: congressman
-         });
+         console.log('http://openstates.org/api/v1/legislators/geo/?lat=' + user.profile.latitude +
+         '&' + 'long=' + user.profile.longitude + '&' + 'apikey=' + Sunlight_API_KEY);
+         urllib.request('http://openstates.org/api/v1/legislators/geo/?lat=' + user.profile.latitude +
+         '&' + 'long=' + user.profile.longitude + '&' + 'apikey=' + Sunlight_API_KEY, function(err, state_data, state_resp) {
+           console.log(state_data.toString()[0]);
+           var stateResults = JSON.parse(state_data.toString());
+           var state_rep = stateResults[0];
+
+
+           if(!err) {
+             res.render('adopt/neighborhood', {
+               title: user.profile.city,
+               state_rep: state_rep.full_name,
+               state_link: state_rep.sources[Math.max(state_rep.sources.length - 1, 0)].url,
+               congressman: congressman.first_name + ' ' + congressman.last_name,
+               congressman_link: congressman.contact_form
+             });
+           }
+         }
+       );
        } else {
          res.render('adopt/neighborhood', {
-           title: 'Your Neighborhood',
+           title: user.profile.city,
          });
        }
       });
     }
-
   } else {
     res.render('home', {
       title: 'Home'
